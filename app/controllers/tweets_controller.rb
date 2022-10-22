eb91b5700b9ab15578575e9f66fa9051bc8bb69c
soyezequiel <alcayaga147147@gmail.com>
2022-10-22 18:18:06 -0300

    Se agrego la posibilidad de subir una foto de perfil nueva

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,23 @@
+class TweetsController < ApplicationController
+
+def index
+  @tweets = Tweet.order(:created_at)
+end
+def show
+  @tweet=Tweet.find(params[:id])
+end
+def new
+      @tweet=Tweet.new
+end
+def create
+  @tweet=Tweet.new(tweet_params)
+  if  @tweet.save
+    redirect_to @tweet, notice: "todo legal"
+  else
+    render :new
+  end
+end
+def tweet_params
+  params.require(:tweet).permit(:content, :monster_id)
+end
+end
--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,3 +1,15 @@
 class Monster < ApplicationRecord
   validates :name, :birthdate, presence: true
-end
+  has_many :tweets
+  validate :numero_de_telefono_tiene_que_ser_valido
+  has_many_attached :image
+
+    def numero_de_telefono_tiene_que_ser_valido
+      if Phone.new(phone).valid?
+        return
+      else
+        errors.add(:phone, :invalid, message: "pepito")
+      end
+
+    end
+  end
