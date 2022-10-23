d014a614e6d431b0ed244068198d8404666a16b0
soyezequiel <alcayaga147147@gmail.com>
2022-10-23 10:36:03 -0300

    Se configuro para que la ubicacion siempre este disponible

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,12 @@
+class LocationsController < ApplicationController
+  def create
+    previous_lat = session[:lat]
+
+    session[:lat] = params[:lat].to_f
+    session[:lng] = params[:lng].to_f
+
+    if previous_lat.nil? && session[:lat].present?
+      redirect_back fallback_location: root_path
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :monsters
   resources :tweets
   root "tweets#index"
+  resources :locations, only: :create
 end
